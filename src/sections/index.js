f77f1b44c59452b9e6bd799731786e8afa0fc164
Mubzz09 <143597850+DevMubzly@users.noreply.github.com>
2024-12-25 02:09:59 +0300

    Second

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero'
+import Subscribe from './Subscribe'
+import Services from './Services'
+import SpecialOffer from './SpecialOffer'
+import SuperQuality from './SuperQuality'
+import Footer from './Footer'
+import PopularProducts from './PopularProducts'
+import CustomerReviews from './CustomerReviews'
+
+export {
+    Hero,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    CustomerReviews,
+    Footer,
+    PopularProducts
+}
